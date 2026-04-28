8b833a836f057cc5deca3ce3b11ee2f7c2885a5a
Saber <asalneshasy3@gmail.com>
2026-04-28 22:57:42 +0300

    Fixed Pydantic V2 warnings and updated test suite

--- src/database.py
+++ src/database.py
@@ -1,6 +1,14 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy import event
+from sqlalchemy.engine import Engine
 
 SQLALCHEMY_DATABASE_URL = "sqlite:///./taskstream.db"
 engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+@event.listens_for(Engine, "connect")
+def set_sqlite_pragma(dbapi_connection, connection_record):
+    cursor = dbapi_connection.cursor()
+    cursor.execute("PRAGMA foreign_keys=ON")
+    cursor.close()
